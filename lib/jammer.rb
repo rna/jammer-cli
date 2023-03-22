8456232911fe3abd5f2b7bb180b0bbed11b221f5
Ramesh Naidu Allu <rameshnaidu.allu@gmail.com>
2023-03-22 13:29:39 +0530

    Fix Typo

--- lib/jammer.rb
+++ lib/jammer.rb
@@ -1,21 +1,21 @@
 module Jammer
   class CLI
     attr_accessor :keyword
-
-    def initialize(keyword = 'TODO')
+    #TODO
+    def initialize(keyword = '#TODO')
       @keyword = keyword
     end
 
     def exists?
-      occurence_count.positive?
+      occurrence_count.positive?
     end
 
-    def occurence_count
+    def occurrence_count
       count_cmd = "grep -Rw $(pwd) -e #{@keyword} | wc -l"
       system(count_cmd)
     end
 
-    def occurence_list
+    def occurrence_list
       search_cmd = "grep -Rw $(pwd) -e #{@keyword}"
       system(search_cmd)
     end
