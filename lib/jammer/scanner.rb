2790307594daaa3a3e56faa073bd3770faaa1007
Ramesh Naidu Allu <rameshnaidu.allu@gmail.com>
2025-09-21 12:29:54 +0530

    Fix for false postives by matching only whole words

--- lib/jammer/scanner.rb
+++ lib/jammer/scanner.rb
@@ -23,12 +23,14 @@ module Jammer
     end
 
     def matches
+      regex_pattern = "\b#{@keyword}\b"
+
       command = if Jammer::Git.inside_work_tree?
-                  # Search staged files (index) to align with pre-commit use case
-                  ['git', 'grep', '-nI', '--cached', '-e', @keyword]
+                  # Search staged files (index) with an extended regex.
+                  ['git', 'grep', '-nI', '-E', '--cached', '-e', regex_pattern]
                 else
-                  # Fallback for non-git directories: recursive, ignore binary, show line numbers
-                  ['grep', '-RIn', @keyword, '.']
+                  # Fallback for non-git directories: recursive, ignore binary, show line numbers, extended regex.
+                  ['grep', '-RInE', regex_pattern, '.']
                 end
 
       stdout, stderr, status = Open3.capture3(*command)
